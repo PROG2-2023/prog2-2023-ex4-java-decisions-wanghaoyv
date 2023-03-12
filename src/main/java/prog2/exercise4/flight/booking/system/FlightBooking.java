0f2d785bfef428a8cc29b0d5da09cd7465f40aac
wanghaoyv <126221103+wanghaoyv@users.noreply.github.com>
2023-03-12 17:33:53 +0800

    Update FlightBooking.java

--- src/main/java/prog2/exercise4/flight/booking/system/FlightBooking.java
+++ src/main/java/prog2/exercise4/flight/booking/system/FlightBooking.java
@@ -1,15 +1,19 @@
-package exercise3.flight.booking.system;
-import java.time.LocalDate;
+package prog2.exercise4.flight.booking.system;
 
-public class FlightBooking 
-{
+import java.time.LocalDate;
+import java.time.Period;
+import java.security.PublicKey;
+import java.util.Random;
+import java.util.Scanner;
+
+public class FlightBooking {
     public String flightCompany;
     public String flightID;
     public String passengerFullName;
-    public String tripSource = "NANJING";
-    public String sourceAirport = "NANJING LUKOU INTERNATIONAL AIRPORT";
-    public String tripDestination = "OULU";
-    public String destinationAirport = "OULU AIRPORT";
+    // public String tripSource = "NANJING";
+    // public String sourceAirport = "NANJING LUKOU INTERNATIONAL AIRPORT";
+    // public String tripDestination = "OULU";
+    // public String destinationAirport = "OULU AIRPORT";
     public LocalDate departureDate;
     public LocalDate returnDate;
     public int childPassengers;
@@ -20,9 +24,8 @@ public class FlightBooking
     public double totalTicketPrice;
     public String ticketNumber;
 
-    public FlightBooking(String passengerFullName, LocalDate depart, LocalDate returnDate, 
-    int childPassengers,int adultPassengers) 
-    {
+    public FlightBooking(String passengerFullName, LocalDate depart, LocalDate returnDate,
+            int childPassengers, int adultPassengers) {
         this.passengerFullName = passengerFullName;
         this.departureDate = depart;
         this.returnDate = returnDate;
@@ -30,163 +33,362 @@ public class FlightBooking
         this.adultPassengers = adultPassengers;
     }
 
-
-    public void setFlightCompany(String flightCompany)
-    {
+    public void setFlightCompany(String flightCompany) {
         this.flightCompany = flightCompany;
     }
-    public String getFlightCompany()
-    {
+
+    public String getFlightCompany() {
 
         return flightCompany;
     }
 
-    public void setFlightID(String flightID)
-    {
+    public void setFlightID(String flightID) {
         this.flightID = flightID;
     }
-    public String getFlightID()
-    {
+
+    public String getFlightID() {
         return flightID;
     }
 
-    public void setPassengerFullName(String passengerFullName)
-    {
+    public void setPassengerFullName(String passengerFullName) {
         this.passengerFullName = passengerFullName;
     }
-    public String getPassengerFullName()
-    {
+
+    public String getPassengerFullName() {
         return passengerFullName;
     }
 
+    // public void setTripSource(String tripSource)
+    // {
+    // this.tripSource = tripSource;
+    // }
+    // public String getTripSource()
+    // {
+    // return tripSource;
+    // }
+
+    // public void setSourceAirport(String sourceAirport)
+    // {
+    // this.sourceAirport= sourceAirport;
+    // }
+    // public String getSourceAirport()
+    // {
+    // return sourceAirport;
+    // }
+
+    // public void setTripDestination (String tripDestination )
+    // {
+    // this.tripDestination = tripDestination ;
+    // }
+    // public String getTripDestination ()
+    // {
+    // return tripDestination ;
+    // }
+
+    // public void setDestinationAirport(String destinationAirport)
+    // {
+    // this.destinationAirport= destinationAirport;
+    // }
+    // public String getDestinationAirport()
+    // {
+    // return destinationAirport;
+    // }
+
+    public void setDepartingDate(LocalDate departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public LocalDate getDepartingDate() {
+        return departureDate;
+    }
+
+    // public void setReturnDate(LocalDate returnDate) {
+    //     this.returnDate = returnDate;
+    // }
+
+    // public LocalDate getReturnDate() {
+    //     return returnDate;
+    // }
+
+    public void setChildrenPassengers(int childPassengers) {
+        this.childPassengers = childPassengers;
+    }
+
+    public int getChildrenPassengers() {
+        return childPassengers;
+    }
+
+    public void setAdultPassengers(int adultPassengers) {
+        this.adultPassengers = adultPassengers;
+    }
+
+    public int getAdultPassengers() {
+        return adultPassengers;
+    }
+
+    public void setTotalPassengers(int childPassengers, int adultPassengers) {
+        this.totalPassengers = childPassengers + adultPassengers;
+    }
+
+    public int getTotalPassengers() {
+        return totalPassengers;
+    }
+
+    public void setDepartingTicketPrice(double departingTicketPrice) {
+        this.departingTicketPrice = departingTicketPrice;
+    }
+
+    public double getDepartingTicketPrice() {
+        return departingTicketPrice;
+    }
+
+    public void setReturnTicketPrice(double returnTicketPrice) {
+        this.returnTicketPrice = returnTicketPrice;
+    }
+
+    public double getReturnTicketPrice() {
+        return returnTicketPrice;
+    }
+
+    public void setTotalTicketPrice(double totalTicketPrice) {
+        this.totalTicketPrice = totalTicketPrice;
+    }
+
+    public double getTotalTicketPrice() {
+        return totalTicketPrice;
+    }
+
+    public void setTicketNumber(String ticketNumber) {
+        this.ticketNumber = ticketNumber;
+    }
+
+    public String getTicketNumber() {
+        return ticketNumber;
+    }
+
+                            // 练习四 添加区
+    // task 2
+
+    private BookingClass booking;
+
+    public enum BookingClass {
+        FIRST, BUSINESS, ECONOMY;
+    }
+
+    public void setBookingClass(int bookingClass)
+    {
+        switch(bookingClass){
+            case 1:
+                this.booking = BookingClass.FIRST;
+                break;
+            case 2:
+                this.booking = BookingClass.BUSINESS;
+                break;
+            case 3:
+                this.booking  = BookingClass.BUSINESS;
+                break;
+        }
+    }
+
+    public BookingClass getBookingClass() {
+        return this.booking;
+    }
+
+    //task 3
+    private TripType trip;
+
+    public enum TripType{
+        RETURN,ONE_WAY;
+    }
+
+    public void setTripType(String tripType)
+    {
+        switch(tripType){
+            case"1":
+                this.trip = TripType.ONE_WAY;
+                break;
+            case"2":
+                this.trip = TripType.RETURN;
+                break;
+        }
+    }
+
+    public TripType getTripType(){
+        return this.trip;
+    }
+
+    //task 4-5
+    private TripSource tripSource;
+    private TripDestination tripDestination;
+    private SourceAirport sourceAirport;
+    private DestinatioAairport destinatioAairport;
+
+    public enum TripSource{
+        NANJING,BEIJING,SHANGHAI,OULU,HELSINKI,PARIS;
+    }
+    public enum TripDestination{
+        NANJING,BEIJING,SHANGHAI,OULU,HELSINKI,PARIS;
+    }
+    public enum SourceAirport{
+        NANJING_LUKOU_INTERNATIONAL_AIRPORT,BEIJING_CAPITAL_INTERNATIONAL_AIRPORT,SHANGHAI_PUDONG_INTERNATIONAL_AIRPORT,OULU_AIRPORT,HELSINKI_AIRPORT,PARIS_CHARLESDE_GAULLE_AIRPORT;
+    }
+    public enum DestinatioAairport{
+        NANJING_LUKOU_INTERNATIONAL_AIRPORT,BEIJING_CAPITAL_INTERNATIONAL_AIRPORT,SHANGHAI_PUDONG_INTERNATIONAL_AIRPORT,OULU_AIRPORT,HELSINKI_AIRPORT,PARIS_CHARLESDE_GAULLE_AIRPORT;
+    }
 
     public void setTripSource(String tripSource)
     {
-        this.tripSource = tripSource;
+        switch(tripSource){
+            case"1":
+                this.tripSource = TripSource.NANJING;
+                break;
+            case"2":
+                this.tripSource = TripSource.BEIJING;
+                break;
+            case"3":
+                this.tripSource = TripSource.SHANGHAI;
+                break;          
+            case"4":
+                this.tripSource = TripSource.OULU;
+                break;          
+            case"5":
+                this.tripSource = TripSource.HELSINKI;
+                break;          
+            case"6":
+                this.tripSource = TripSource.PARIS;
+                break;          
+        }
     }
-    public String getTripSource()
+    public TripSource getTripSource(){
+        return this.tripSource;
+    }
+
+
+    public void setTripDestination(String tripDestination)
     {
-        return tripSource;
+        switch(tripDestination){
+            case"1":
+                this.tripDestination = TripDestination.NANJING;
+                break;
+            case"2":
+                this.tripDestination = TripDestination.BEIJING;
+                break;
+            case"3":
+                this.tripDestination = TripDestination.SHANGHAI;
+                break;          
+            case"4":
+                this.tripDestination = TripDestination.OULU;
+                break;          
+            case"5":
+                this.tripDestination = TripDestination.HELSINKI;
+                break;          
+            case"6":
+                this.tripDestination = TripDestination.PARIS;
+                break;          
+        }
+    }
+    public TripDestination getTripDestination(){
+        return this.tripDestination;
     }
 
     public void setSourceAirport(String sourceAirport)
     {
-        this.sourceAirport= sourceAirport;
+        switch(sourceAirport){
+            case"1":
+                this.sourceAirport = SourceAirport.NANJING_LUKOU_INTERNATIONAL_AIRPORT;
+                break;
+            case"2":
+                this.sourceAirport = SourceAirport.BEIJING_CAPITAL_INTERNATIONAL_AIRPORT;
+                break;
+            case"3":
+                this.sourceAirport = SourceAirport.SHANGHAI_PUDONG_INTERNATIONAL_AIRPORT;
+                break;
+            case"4":
+                this.sourceAirport = SourceAirport.OULU_AIRPORT;
+                break;
+            case"5":
+                this.sourceAirport = SourceAirport.HELSINKI_AIRPORT;
+                break;
+            case"6":
+                this.sourceAirport = SourceAirport.PARIS_CHARLESDE_GAULLE_AIRPORT;
+                break;
+        }
     }
-    public String getSourceAirport()
-    {
-        return sourceAirport;
+    public SourceAirport getSourceAirport(){
+        return this.sourceAirport;
     }
 
-    public void setTripDestination (String tripDestination )
+    public void DestinatioAairport(String destinatioAairport)
     {
-        this.tripDestination = tripDestination ;
+        switch(destinatioAairport){
+            case"1":
+                this.destinatioAairport = DestinatioAairport.NANJING_LUKOU_INTERNATIONAL_AIRPORT;
+                break;
+            case"2":
+                this.destinatioAairport = DestinatioAairport.BEIJING_CAPITAL_INTERNATIONAL_AIRPORT;
+                break;
+            case"3":
+                this.destinatioAairport = DestinatioAairport.SHANGHAI_PUDONG_INTERNATIONAL_AIRPORT;
+                break;          
+            case"4":
+                this.destinatioAairport = DestinatioAairport.OULU_AIRPORT;
+                break;          
+            case"5":
+                this.destinatioAairport = DestinatioAairport.HELSINKI_AIRPORT;
+                break;          
+            case"6":
+                this.destinatioAairport = DestinatioAairport.PARIS_CHARLESDE_GAULLE_AIRPORT;
+                break;          
+        }
     }
-    public String getTripDestination ()
-    {
-        return tripDestination ;
+    public DestinatioAairport get(){
+        return this.destinatioAairport;
     }
 
-    public void setDestinationAirport(String destinationAirport)
-    {
-        this.destinationAirport= destinationAirport;
-    }
-    public String getDestinationAirport()
-    {
-        return destinationAirport;
-    }
+    //task 6
 
-    public void setDepartingDate(LocalDate departureDate)
-    {
-        this.departureDate= departureDate;
+    public void setDepartureDate(LocalDate departureDate){
+        String sdepart = "2023-03-04";
+        this.departureDate = LocalDate.parse(sdepart);
     }
-    public LocalDate  getDepartingDate()
-    {
+    public LocalDate getDepartureDate(){
         return departureDate;
     }
 
-    public void setReturnDate(LocalDate returnDate)
-    {
-        this.returnDate= returnDate;
+    public void setReturnDate(LocalDate returnDate){
+        String sreturn = "2023-03-05";
+        this.returnDate = departureDate.plusDays(2);
+        long i = departureDate.toEpochDay() -returnDate.toEpochDay();
+        if(i==0){
+            LocalDate reDay=departureDate.plusDays(2);
+        }else if(i==1){
+            LocalDate reDay=departureDate.plusDays(1);
+        }
     }
-    public LocalDate getReturnDate()
-    {
+    public LocalDate getReturnDate(){
         return returnDate;
     }
 
-    public void setChildrenPassengers(int childPassengers)
-    {
-        this.childPassengers= childPassengers;
-    }
-    public int getChildrenPassengers()
-    {
-        return childPassengers;
+    //task 7
+    
+
+
+
+
+
+                            // 练习四 添加区
+
+    public String toString() {
+        return "Dear " + passengerFullName + ". Thank you for booking your flight with " +
+                flightCompany + ". Following are the details of your booking and the trip:" + "\n" +
+                "Ticket Number: " + ticketNumber + "\n" +
+                "From " + tripSource + " to " + tripDestination + "\n" +
+                "Date of departure: " + departureDate + "\n" +
+                "Date of return: " + returnDate + "\n" +
+                "Total passengers: " + totalPassengers + "\n" +
+                "Total ticket price in Euros: " + totalTicketPrice;
     }
 
-    public void setAdultPassengers(int adultPassengers)
-    {
-        this.adultPassengers= adultPassengers;
-    }
-    public int getAdultPassengers()
-    {
-        return adultPassengers;
-    }
-
-    public void setTotalPassengers(int childPassengers,int adultPassengers)
-    {
-        this.totalPassengers= childPassengers+adultPassengers;
-    }
-    public int getTotalPassengers()
-    {
-        return totalPassengers;
-    }
-
-    public void setDepartingTicketPrice(double departingTicketPrice)
-    {
-        this.departingTicketPrice = departingTicketPrice;
-    }
-    public double getDepartingTicketPrice()
-    {
-        return departingTicketPrice;
-    }
-
-    public void setReturnTicketPrice(double returnTicketPrice)
-    {
-        this.returnTicketPrice = returnTicketPrice;
-    }
-    public double getReturnTicketPrice()
-    {
-        return returnTicketPrice;
-    }
-
-    public void setTotalTicketPrice(double totalTicketPrice)
-    {
-        this.totalTicketPrice = totalTicketPrice;
-    }
-    public double getTotalTicketPrice()
-    {
-        return totalTicketPrice;
-    }
-
-    public void setTicketNumber(String ticketNumber)
-    {
-        this.ticketNumber = ticketNumber;
-    }
-    public String getTicketNumber()
-    {
-        return ticketNumber;
-    }
-
-    public String toString()
-    {
-        return "Dear " + passengerFullName + ". Thank you for booking your flight with " + 
-        flightCompany + ". Following are the details of your booking and the trip:" + "\n" + 
-        "Ticket Number: " + ticketNumber + "\n" + 
-        "From " + tripSource + " to " + tripDestination + "\n" +
-        "Date of departure: " + departureDate + "\n" +
-        "Date of return: " + returnDate + "\n" +
-        "Total passengers: " + totalPassengers + "\n" +
-        "Total ticket price in Euros: " + totalTicketPrice;
-    }
+}
 
 }
